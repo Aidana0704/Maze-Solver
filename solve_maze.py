fdb9fed17adf1baab32920ef31497c213b628c04
alrightytighty <ileikpotatozzzz@gmail.com>
2026-04-22 10:56:35 -0500

    ready 2 presentgit add .!

--- solve_maze.py
+++ solve_maze.py
@@ -13,20 +13,13 @@ class CellAStarInfo:
         self.cell: MazeCell = cell
         self.cost: int = cost
         self.coords: tuple[int, int] = coords
-    
+
     def f(self):
         return self.cost + manhattan_distance(self.coords, CellAStarInfo.goal_coords)
 
     def __lt__(self, other: CellAStarInfo):
         return self.f() < other.f()
-    
 
-# start pos and goal pos to be interpreted as (row, column) and everything else is to be interpreted as (x, y)
-# y decreases upwards, x increases rightwards
-# don't ask me why i did it this way. i'm tired chief. i'm tired.
-
-# (it's because that's how pygame works)
-# i'm a fucking chud bro
 
 def get_start_and_goal_pos(maze: list[list[MazeCell]]) -> tuple[tuple[int, int], tuple[int, int]]:
     goal = None
@@ -37,15 +30,11 @@ def get_start_and_goal_pos(maze: list[list[MazeCell]]) -> tuple[tuple[int, int],
                 goal = (rowNum, colNum)
             elif cell.state == CellState.START:
                 start = (rowNum, colNum)
-            
+
             if (goal is not None and start is not None):
                 return (start, goal)
-    
 
     return (start, goal)
-
-# THIS IS AN ALGORITHM PURELY USED FOR THE VISUAL EXPLORATION OF THE MAZE.
-# IT HAS NOTHING TO DO WITH THE A* BENEATH.
 
 def get_path_between_expanded_nodes(current_node: CellAStarInfo, next_node: CellAStarInfo):
     next_node_ancestors = set()
@@ -90,9 +79,8 @@ def solve_maze_astar(maze: list[list[MazeCell]], start_pos: tuple[int, int], goa
             while c is not None:
                 ans.append(c.coords)
                 c = c.parent
-            
+
             return ans
-            
 
         if current_node_cell.bottom_square is not None:
             heappush(q, CellAStarInfo(current_node_cell.bottom_square, current_node.cost + 1, current_node_cell.bottom_square.position, current_node))
@@ -103,13 +91,7 @@ def solve_maze_astar(maze: list[list[MazeCell]], start_pos: tuple[int, int], goa
         if current_node_cell.top_square is not None:
             heappush(q, CellAStarInfo(current_node_cell.top_square, current_node.cost + 1, current_node_cell.top_square.position, current_node))
 
-    return [] # no path was found
-
-# class used for step by step solving of the problem
-# again, initial and end position is expected to be (row, column) when passed
-# to constructor, but is automatically converted during the constructor
-
-# i loooovve being inconsistent
+    return []
 
 class AStarSolver:
     def __init__(self, graph: list[list[MazeCell]]):
@@ -136,7 +118,7 @@ class AStarSolver:
     def _move_to(self, cell: MazeCell):
         if self.started:
             return
-        
+
         if (cell.state == CellState.HAZARD):
             self.current_node = self.start_node
             self.character_position = self.current_node.coords
@@ -145,16 +127,15 @@ class AStarSolver:
 
         if (cell.state == CellState.CONFUSION):
             self.confusionSteps = 5
-        
+
         self.current_node = CellAStarInfo(cell, 0, cell.position)
         self.character_position = cell.position
         self.current_pos = cell.position
 
-
     _OPPOSITES = {
-        'left_square':   'right_square',
-        'right_square':  'left_square',
-        'top_square':    'bottom_square',
+        'left_square': 'right_square',
+        'right_square': 'left_square',
+        'top_square': 'bottom_square',
         'bottom_square': 'top_square',
     }
 
@@ -166,10 +147,17 @@ class AStarSolver:
         if cell is not None:
             self._move_to(cell)
 
-    def move_left(self):  self._move('left_square')
-    def move_right(self): self._move('right_square')
-    def move_up(self):    self._move('top_square')
-    def move_down(self):  self._move('bottom_square')
+    def move_left(self):
+        self._move('left_square')
+
+    def move_right(self):
+        self._move('right_square')
+
+    def move_up(self):
+        self._move('top_square')
+
+    def move_down(self):
+        self._move('bottom_square')
 
     def step(self):
         if (self.finished):
@@ -187,11 +175,9 @@ class AStarSolver:
         if (current_node.coords in self.visited):
             return
 
-
         self.character_path = get_path_between_expanded_nodes(self.current_node, current_node)
         self.current_node = current_node
         self.current_pos = current_node.coords
-
 
         self.visited.add(current_node.coords)
 
@@ -210,7 +196,6 @@ class AStarSolver:
             self.character_position = self.start_pos
             return
 
-
         if current_node_cell.bottom_square is not None:
             heappush(self.queue, CellAStarInfo(current_node_cell.bottom_square, current_node.cost + 1, current_node_cell.bottom_square.position, current_node))
         if current_node_cell.right_square is not None:
@@ -219,6 +204,3 @@ class AStarSolver:
             heappush(self.queue, CellAStarInfo(current_node_cell.left_square, current_node.cost + 1, current_node_cell.left_square.position, current_node))
         if current_node_cell.top_square is not None:
             heappush(self.queue, CellAStarInfo(current_node_cell.top_square, current_node.cost + 1, current_node_cell.top_square.position, current_node))
-
-
-
